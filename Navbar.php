f90bfb2429cd3ffe1496ff7d8d4676078dff76f2
IZulz <39718158+IZulz@users.noreply.github.com>
2021-05-29 14:55:12 +0700

    navbar fix

--- Navbar.php
+++ Navbar.php
@@ -6,6 +6,9 @@
         <script src="/Settings/js/bootstrap.min.js"></script>
 
         <style>
+            .navbar{
+                background: rgba(255, 255, 255, 0.6);
+            }
             .navbar-nav {
                 font-size: 1.5rem;
             }
@@ -57,7 +60,7 @@
                 display: block;
             }
         </style>
-        <nav class="navbar navbar-expand-lg navbar-light bg-transparent mb-5">
+        <nav class="navbar navbar-expand-lg navbar-light sticky-top mb-5">
             <a class="navbar-brand" href="/">
                 <i class="fa fa-tree text-success"></i>
                 Chacha<text class="text-success">Villa</text>
@@ -100,12 +103,22 @@
                         }else{
 
                             echo'
-                            <a href="Register.php" class="nav-link h5">
-                                Daftar
-                            </a>
-                            <a href="Login.php" class="nav-link h5">
-                                Login
-                            </a>';
+
+                            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                                <li class="nav-item active">
+                                    <a href="Register.php" class="nav-link h5">
+                                        Daftar
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="Login.php" class="nav-link h5">
+                                        Login
+                                    </a>
+                                </li>
+                            </ul>
+
+                           
+                           ';
                         
                         }
                         ?>
