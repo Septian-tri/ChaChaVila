7fce930d83949fc063a21fb43eb497bc5edaca0f
IZulz <39718158+IZulz@users.noreply.github.com>
2021-06-13 18:20:43 +0700

    pembersihan code

--- Detail_villa.php
+++ Detail_villa.php
@@ -159,123 +159,7 @@ if(!preg_match('/^[V]{1}[I]{1}[D]{1}[\=]{1}[a-zA-Z0-9]{15,}$/', $_SERVER['QUERY_
 
             .btn-group img{
                 width : 100%;
-            }
-
-            /* Styles the thumbnail */
-
-            /* a.lightbox img {
-            width   : 30%;
-            height  : auto;
-            border  : 3px solid white;
-            box-shadow: 0px 0px 8px rgba(0,0,0,.3);
-            display: block;
-            } */
-
-            /* Styles the lightbox, removes it from sight and adds the fade-in transition */
-
-            /* .lightbox-target {
-            position: fixed;
-            top: -100%;
-            width: 100%;
-            background: rgba(0,0,0,.7);
-            width: 100%;
-            opacity: 0;
-            -webkit-transition: opacity .5s ease-in-out;
-            -moz-transition: opacity .5s ease-in-out;
-            -o-transition: opacity .5s ease-in-out;
-            transition: opacity .5s ease-in-out;
-            overflow: hidden;
-            } */
-
-            /* Styles the lightbox image, centers it vertically and horizontally, adds the zoom-in transition and makes it responsive using a combination of margin and absolute positioning */
-
-            /* .lightbox-target img {
-            margin: auto;
-            position: absolute;
-            top: 0;
-            left:0;
-            right:0;
-            bottom: 0;
-            max-height: 0%;
-            max-width: 0%;
-            border: 3px solid white;
-            box-shadow: 0px 0px 8px rgba(0,0,0,.3);
-            box-sizing: border-box;
-            -webkit-transition: .5s ease-in-out;
-            -moz-transition: .5s ease-in-out;
-            -o-transition: .5s ease-in-out;
-            transition: .5s ease-in-out;
-            } */
-
-            /* Styles the close link, adds the slide down transition */
-
-            /* a.lightbox-close {
-            display: block;
-            width:50px;
-            height:50px;
-            box-sizing: border-box;
-            background: white;
-            color: black;
-            text-decoration: none;
-            position: absolute;
-            top: -80px;
-            right: 0;
-            -webkit-transition: .5s ease-in-out;
-            -moz-transition: .5s ease-in-out;
-            -o-transition: .5s ease-in-out;
-            transition: .5s ease-in-out;
-            } */
-
-            /* Provides part of the "X" to eliminate an image from the close link */
-
-            /* a.lightbox-close:before {
-            content: "";
-            display: block;
-            height: 30px;
-            width: 1px;
-            background: black;
-            position: absolute;
-            left: 26px;
-            top:10px;
-            -webkit-transform:rotate(45deg);
-            -moz-transform:rotate(45deg);
-            -o-transform:rotate(45deg);
-            transform:rotate(45deg);
-            } */
-
-            /* Provides part of the "X" to eliminate an image from the close link */
-
-            /* a.lightbox-close:after {
-            content: "";
-            display: block;
-            height: 30px;
-            width: 1px;
-            background: black;
-            position: absolute;
-            left: 26px;
-            top:10px;
-            -webkit-transform:rotate(-45deg);
-            -moz-transform:rotate(-45deg);
-            -o-transform:rotate(-45deg);
-            transform:rotate(-45deg);
-            } */
-
-            /* Uses the :target pseudo-class to perform the animations upon clicking the .lightbox-target anchor */
-
-            /* .lightbox-target:target {
-            opacity: 1;
-            top: 0;
-            bottom: 0;
-            }
-
-            .lightbox-target:target img {
-            max-height: 100%;
-            max-width: 100%;
-            }
-
-            .lightbox-target:target a.lightbox-close {
-            top: 0px;
-            } */
+            }           
 
             .typeGambar{
                 width : 100%;
@@ -297,7 +181,7 @@ if(!preg_match('/^[V]{1}[I]{1}[D]{1}[\=]{1}[a-zA-Z0-9]{15,}$/', $_SERVER['QUERY_
             .box {
             flex: 1;
             overflow: hidden;
-            transition: .5s;
+            transition: .3s;
             /* margin: 0 2%; */
             box-shadow: 0 20px 30px rgba(0,0,0,.1);
             line-height: 0;
@@ -310,15 +194,8 @@ if(!preg_match('/^[V]{1}[I]{1}[D]{1}[\=]{1}[a-zA-Z0-9]{15,}$/', $_SERVER['QUERY_
             transition: .5s;
             }
 
-            /* .box > span {
-            font-size: 3.8vh;
-            display: block;
-            text-align: center;
-            height: 10vh;
-            line-height: 2.6;
-            } */
             @media (max-width: 600px) {
-                .box:active{ flex: 1 1 60%; }
+                .box:active{ flex: 60%; }
                 .box > img{
                     pointer-events: none;
                 }
@@ -332,7 +209,6 @@ if(!preg_match('/^[V]{1}[I]{1}[D]{1}[\=]{1}[a-zA-Z0-9]{15,}$/', $_SERVER['QUERY_
                     height: 80vh;
                 }
             }
-            /* .box:hover { flex: 1 1 20%; } */
             .box:hover > img {
             width: 100%;
             height: 100%;
@@ -475,30 +351,6 @@ if(!preg_match('/^[V]{1}[I]{1}[D]{1}[\=]{1}[a-zA-Z0-9]{15,}$/', $_SERVER['QUERY_
     
             </div>
         </div>
-
-        
-        <!-- modal -->
-        <!-- <div class="lightbox-target" id="1">
-            <img src="http://placehold.it/700x400"/>
-            <a class="lightbox-close" href="#"></a>
-        </div>
-        <div class="lightbox-target" id="2">
-            <img src="http://placehold.it/400x400"/>
-            <a class="lightbox-close" href="#"></a>
-        </div>
-        <div class="lightbox-target" id="3">
-            <img src="http://placehold.it/700x400"/>
-            <a class="lightbox-close" href="#"></a>
-        </div>
-        <div class="lightbox-target" id="4">
-            <img src="http://placehold.it/400x400"/>
-            <a class="lightbox-close" href="#"></a>
-        </div>
-        <div class="lightbox-target" id="5">
-            <img src="http://placehold.it/400x400"/>
-            <a class="lightbox-close" href="#"></a>
-        </div> -->
-        <!-- modal -->
 
         <div class="container">
             <div class="rounded bg-white mt-3 p-3">
--- Foto_villa.php
+++ Foto_villa.php
@@ -7,11 +7,15 @@
 
     <title>Foto Fotot</title>
     <link rel="stylesheet" href="/Settings/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/settings/font-awesome/css/font-awesome.min.css">        
+    <link rel="stylesheet" href="/settings/font-awesome/css/font-awesome.min.css">
+
+    <!-- untuk sementara masih cdn -->
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
 
     <script src="/Settings/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="/Settings/js/jquery-3.2.1.slim.min.js"></script>
+
+    <!-- untuk sementara masih cdn -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
 
 
@@ -30,7 +34,6 @@
             overflow: hidden;
         }
 
-
         .green {
             background-color: #6fb936;
         }
@@ -47,7 +50,6 @@
             img.zoom {
                 width: 100%;
             }
-
         }
 
         @media (min-width: 601px) {
@@ -55,13 +57,12 @@
                 width: 20%;
             }
 
-            .test {
+            .cener {
                 padding-left: 13%;
             }
         }
 
         img.zoom {
-
             height: 200px;
             border-radius: 5px;
             object-fit: cover;
@@ -72,7 +73,6 @@
             margin-bottom: 5px;
         }
 
-
         .transition {
             -webkit-transform: scale(1.2);
             -moz-transform: scale(1.2);
@@ -81,7 +81,6 @@
         }
 
         .modal-header {
-
             border-bottom: none;
         }
 
@@ -91,27 +90,6 @@
 
         .modal-footer {
             display: none;
-        }
-
-        div.gallery {
-            margin: 5px;
-            border: 1px solid #ccc;
-            float: left;
-            width: 180px;
-        }
-
-        div.gallery:hover {
-            border: 1px solid #777;
-        }
-
-        div.gallery img {
-            width: 100%;
-            height: auto;
-        }
-
-        div.desc {
-            padding: 15px;
-            text-align: center;
         }
     </style>
 
@@ -139,7 +117,15 @@
         <div class="kembali lightbox">
             <i class="fa fa-arrow-left fa-3x text-white mb-3" aria-hidden="true"></i>
         </div>
-        <div class="test">
+        <div class="cener">
+
+            <!-- tinggal panggil looping nya disini -->
+            <!-- cara pake nya gini href dan src harus sama, sesuai dengan cara manggil img nya -->
+
+            <!-- <a href="linknya gan harus sama" class="fancybox" rel="ligthbox">
+                <img src="linknya gan harus sama" class="zoom img-fluid " alt="">
+            </a> -->
+
             <a href="https://images.pexels.com/photos/62307/air-bubbles-diving-underwater-blow-62307.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="fancybox" rel="ligthbox">
                 <img src="https://images.pexels.com/photos/62307/air-bubbles-diving-underwater-blow-62307.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="zoom img-fluid " alt="">
             </a>
@@ -166,7 +152,150 @@
             </a>
         </div>
     </div>
-    <script src="Settings/js/main.js"></script>
 </body>
+
+    <!-- MODAL ABAL ABAL NITIP DULU -->
+
+    <!-- modal -->
+    <!-- <div class="lightbox-target" id="1">
+        <img src="http://placehold.it/700x400"/>
+        <a class="lightbox-close" href="#"></a>
+    </div>
+    <div class="lightbox-target" id="2">
+        <img src="http://placehold.it/400x400"/>
+        <a class="lightbox-close" href="#"></a>
+    </div>
+    <div class="lightbox-target" id="3">
+        <img src="http://placehold.it/700x400"/>
+        <a class="lightbox-close" href="#"></a>
+    </div>
+    <div class="lightbox-target" id="4">
+        <img src="http://placehold.it/400x400"/>
+        <a class="lightbox-close" href="#"></a>
+    </div>
+    <div class="lightbox-target" id="5">
+        <img src="http://placehold.it/400x400"/>
+        <a class="lightbox-close" href="#"></a>
+    </div> -->
+    <!-- modal -->
+<style>
+    /* STYLE BODONG JANGAN DULU DI HAPUS */
+
+    /* Styles the thumbnail */
+
+    /* a.lightbox img {
+            width   : 30%;
+            height  : auto;
+            border  : 3px solid white;
+            box-shadow: 0px 0px 8px rgba(0,0,0,.3);
+            display: block;
+            } */
+
+    /* Styles the lightbox, removes it from sight and adds the fade-in transition */
+
+    /* .lightbox-target {
+            position: fixed;
+            top: -100%;
+            width: 100%;
+            background: rgba(0,0,0,.7);
+            width: 100%;
+            opacity: 0;
+            -webkit-transition: opacity .5s ease-in-out;
+            -moz-transition: opacity .5s ease-in-out;
+            -o-transition: opacity .5s ease-in-out;
+            transition: opacity .5s ease-in-out;
+            overflow: hidden;
+            } */
+
+    /* Styles the lightbox image, centers it vertically and horizontally, adds the zoom-in transition and makes it responsive using a combination of margin and absolute positioning */
+
+    /* .lightbox-target img {
+            margin: auto;
+            position: absolute;
+            top: 0;
+            left:0;
+            right:0;
+            bottom: 0;
+            max-height: 0%;
+            max-width: 0%;
+            border: 3px solid white;
+            box-shadow: 0px 0px 8px rgba(0,0,0,.3);
+            box-sizing: border-box;
+            -webkit-transition: .5s ease-in-out;
+            -moz-transition: .5s ease-in-out;
+            -o-transition: .5s ease-in-out;
+            transition: .5s ease-in-out;
+            } */
+
+    /* Styles the close link, adds the slide down transition */
+
+    /* a.lightbox-close {
+            display: block;
+            width:50px;
+            height:50px;
+            box-sizing: border-box;
+            background: white;
+            color: black;
+            text-decoration: none;
+            position: absolute;
+            top: -80px;
+            right: 0;
+            -webkit-transition: .5s ease-in-out;
+            -moz-transition: .5s ease-in-out;
+            -o-transition: .5s ease-in-out;
+            transition: .5s ease-in-out;
+            } */
+
+    /* Provides part of the "X" to eliminate an image from the close link */
+
+    /* a.lightbox-close:before {
+            content: "";
+            display: block;
+            height: 30px;
+            width: 1px;
+            background: black;
+            position: absolute;
+            left: 26px;
+            top:10px;
+            -webkit-transform:rotate(45deg);
+            -moz-transform:rotate(45deg);
+            -o-transform:rotate(45deg);
+            transform:rotate(45deg);
+            } */
+
+    /* Provides part of the "X" to eliminate an image from the close link */
+
+    /* a.lightbox-close:after {
+            content: "";
+            display: block;
+            height: 30px;
+            width: 1px;
+            background: black;
+            position: absolute;
+            left: 26px;
+            top:10px;
+            -webkit-transform:rotate(-45deg);
+            -moz-transform:rotate(-45deg);
+            -o-transform:rotate(-45deg);
+            transform:rotate(-45deg);
+            } */
+
+    /* Uses the :target pseudo-class to perform the animations upon clicking the .lightbox-target anchor */
+
+    /* .lightbox-target:target {
+            opacity: 1;
+            top: 0;
+            bottom: 0;
+            }
+
+            .lightbox-target:target img {
+            max-height: 100%;
+            max-width: 100%;
+            }
+
+            .lightbox-target:target a.lightbox-close {
+            top: 0px;
+            } */
+</style>
 
 </html>
